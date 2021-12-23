7e8225461c6657b66b275aefa16010013bc681ff
Arjun <arjundoel@gmail.com>
2021-12-23 12:44:51 +0000

    updating saved set

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import './random_words.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,46 +13,5 @@ class MyApp extends StatelessWidget {
             colorScheme: const ColorScheme.light()
                 .copyWith(primary: Colors.purple[900])),
         home: const RandomWords());
-  }
-}
-
-class RandomWords extends StatefulWidget {
-  const RandomWords({Key? key}) : super(key: key);
-
-  @override
-  _RandomWordsState createState() => _RandomWordsState();
-}
-
-class _RandomWordsState extends State<RandomWords> {
-  // Generated random word pair from enslish package.
-  final _randomWordPairs = <WordPair>[];
-
-  Widget _buildList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(8),
-      itemBuilder: (context, item) {
-        if (item.isOdd) return const Divider();
-
-        final index = item ~/ 2;
-
-        if (index >= _randomWordPairs.length) {
-          _randomWordPairs.addAll(generateWordPairs().take(20));
-        }
-
-        return _buildRow(_randomWordPairs[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-        title: Text(pair.asPascalCase, style: const TextStyle(fontSize: 18.0)));
-  }
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('WordPair Generator')),
-      body: _buildList(),
-    );
   }
 }
---
+++ lib/random_words.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  // Generated random word pair from enslish package.
+  final _randomWordPairs = <WordPair>[];
+  // Save word pairs here:
+  final _savedWordPairs = Set<WordPair>();
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return const Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(10));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _savedWordPairs.contains(pair);
+    return ListTile(
+        title: Text(pair.asPascalCase, style: const TextStyle(fontSize: 18.0)),
+        trailing: Icon(alreadySaved ? Icons.favorite : Icons.favorite_border,
+            color: alreadySaved ? Colors.red : null),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _savedWordPairs.remove(pair);
+            } else {
+              _savedWordPairs.add(pair);
+            }
+          });
+        });
+  }
+
+  void _pushSaved() {}
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('WordPair Generator'), actions: <Widget>[
+        IconButton(icon: const Icon(Icons.list), onPressed: _pushSaved)
+      ]),
+      body: _buildList(),
+    );
+  }
+}
