1730d041bdd6d0c83247bee20926dc0a7c316f03
Arjun <arjundoel@gmail.com>
2021-12-23 12:31:45 +0000

    added in list view widget

--- lib/main.dart
+++ lib/main.dart
@@ -8,14 +8,51 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // Generated random word pair from enslish package.
-    final wordPair = WordPair.random();
     return MaterialApp(
         theme: ThemeData(
             colorScheme: const ColorScheme.light()
                 .copyWith(primary: Colors.purple[900])),
-        home: Scaffold(
-            appBar: AppBar(title: const Text('WordPair Generator')),
-            body: Center(child: Text(wordPair.asPascalCase))));
+        home: const RandomWords());
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  // Generated random word pair from enslish package.
+  final _randomWordPairs = <WordPair>[];
+
+  Widget _buildList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8),
+      itemBuilder: (context, item) {
+        if (item.isOdd) return const Divider();
+
+        final index = item ~/ 2;
+
+        if (index >= _randomWordPairs.length) {
+          _randomWordPairs.addAll(generateWordPairs().take(20));
+        }
+
+        return _buildRow(_randomWordPairs[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+        title: Text(pair.asPascalCase, style: const TextStyle(fontSize: 18.0)));
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('WordPair Generator')),
+      body: _buildList(),
+    );
   }
 }
