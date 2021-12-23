ee9a3bb254ada57d395e38c080e6f518136fa086
Arjun <arjundoel@gmail.com>
2021-12-23 11:52:20 +0000

    color scheme change

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(primaryColor: Colors.purple[900]),
+        theme: ThemeData(
+            colorScheme: const ColorScheme.light()
+                .copyWith(primary: Colors.purple[900])),
         home: Scaffold(
             appBar: AppBar(title: const Text('WordPair Generator')),
             body: const Center(child: Text('Hello World'))));
